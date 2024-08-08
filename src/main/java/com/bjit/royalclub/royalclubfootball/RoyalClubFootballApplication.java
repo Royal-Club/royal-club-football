8444abc310cc6fd7b829e6b8caa33b5b34059684
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2024-08-08 16:43:58 +0600

    initiate with base dependencies
    -- added starter-web, jpa, flyway, mysql, lombok dependencies.

---
+++ src/main/java/com/bjit/royalclub/royalclubfootball/RoyalClubFootballApplication.java
@@ -0,0 +1,13 @@
+package com.bjit.royalclub.royalclubfootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalClubFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalClubFootballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjit/royalclub/royalclubfootball/RoyalClubFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjit.royalclub.royalclubfootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalClubFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
