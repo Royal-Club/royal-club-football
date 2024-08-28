e5a08ef3a1ba87127de292ee43ef3918d6d7a0b1
AktaruzzamanRakib <aktaruzzaman.rakib@bjitgroup.com>
2024-08-28 12:33:45 +0600

    avoid NON_EMPTY empty response issue

--- src/main/java/com/bjit/royalclub/royalclubfootball/model/PlayerParticipationResponse.java
+++ src/main/java/com/bjit/royalclub/royalclubfootball/model/PlayerParticipationResponse.java
@@ -1,6 +1,5 @@
 package com.bjit.royalclub.royalclubfootball.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class PlayerParticipationResponse {
     private Long playerId;
     private String playerName;
