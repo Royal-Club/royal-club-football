fe05d317c9d9dd6a0e9f5b14bed01ef4d05643c0
Rakib <rakibccj@gmail.com>
2024-09-01 09:49:06 +0600

    0 time goalkeeper tract added in goalkeeper stats API

--- src/main/java/com/bjit/royalclub/royalclubfootball/repository/TeamPlayerRepository.java
+++ src/main/java/com/bjit/royalclub/royalclubfootball/repository/TeamPlayerRepository.java
@@ -16,10 +16,11 @@ public interface TeamPlayerRepository extends JpaRepository<TeamPlayer, Long> {
     @Query("SELECT tp FROM TeamPlayer tp WHERE tp.team.id = :teamId AND tp.player.id = :playerId")
     Optional<TeamPlayer> findByTeamIdAndPlayerId(@Param("teamId") Long teamId, @Param("playerId") Long playerId);
 
-    @Query("SELECT new com.bjit.royalclub.royalclubfootball.model.GoalkeeperStatsResponse(tp.player.id, tp.player.name, COUNT(tp)) " +
-            "FROM TeamPlayer tp " +
-            "WHERE tp.playingPosition = 'GOALKEEPER' AND tp.player.id IN :playerIds " +
-            "GROUP BY tp.player.id, tp.player.name " +
+    @Query("SELECT new com.bjit.royalclub.royalclubfootball.model.GoalkeeperStatsResponse(p.id, p.name, COUNT(tp)) " +
+            "FROM Player p " +
+            "LEFT JOIN TeamPlayer tp ON p.id = tp.player.id AND tp.playingPosition = 'GOALKEEPER' " +
+            "WHERE p.id IN :playerIds " +
+            "GROUP BY p.id, p.name " +
             "ORDER BY COUNT(tp) ASC")
     List<GoalkeeperStatsResponse> findGoalkeeperStatsByPlayerIds(@Param("playerIds") List<Long> playerIds);
 
