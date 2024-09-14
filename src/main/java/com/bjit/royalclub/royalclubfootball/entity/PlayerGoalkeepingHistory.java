482d87b54170e47789bd1a8884899f9c37d9aad3
Rakib <rakibccj@gmail.com>
2024-09-14 22:38:48 +0600

    Added PlayerGoalkeepingHistory sql and entity

---
+++ src/main/java/com/bjit/royalclub/royalclubfootball/entity/PlayerGoalkeepingHistory.java
@@ -0,0 +1,44 @@
+package com.bjit.royalclub.royalclubfootball.entity;
+
+
+import com.bjit.royalclub.royalclubfootball.entity.audit.AuditBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "player_goalkeeping_history")
+public class PlayerGoalkeepingHistory extends AuditBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "player_id", nullable = false)
+    private Player player;
+
+    @Column(name = "round_number", nullable = false)
+    private int roundNumber;/*GoalKeeping round number*/
+
+    @Column(name = "played_date", nullable = false)
+    private LocalDateTime playedDate;
+}
---
+++ src/main/resources/db/migration/V20__create_player_goalkeeping_history .sql
@@ -0,0 +1,12 @@
+CREATE TABLE player_goalkeeping_history
+(
+    id               BIGINT PRIMARY KEY AUTO_INCREMENT,
+    player_id        BIGINT   NOT NULL,
+    round_number     INT      NOT NULL,
+    played_date      DATETIME NOT NULL,
+    created_by       BIGINT   NOT NULL,
+    created_date     DATETIME NOT NULL,
+    last_modified_by BIGINT,
+    updated_date     DATETIME DEFAULT NULL,
+    FOREIGN KEY (player_id) REFERENCES players (id)
+);
