8f36b116c64068da27e4d3a6792bbe4a042c88c5
aktaruzzaman.rakib <aktaruzzaman.rakib@bjitgroup.com>
2024-11-07 12:29:59 +0600

    Spring, is unable to deserialize the JSON payload into an instance of ClubRuleRequest

--- src/main/java/com/bjit/royalclub/royalclubfootball/model/ClubRuleRequest.java
+++ src/main/java/com/bjit/royalclub/royalclubfootball/model/ClubRuleRequest.java
@@ -1,11 +1,15 @@
 package com.bjit.royalclub.royalclubfootball.model;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClubRuleRequest {
     @NotBlank(message = "rule description is required.")
     private String description;
